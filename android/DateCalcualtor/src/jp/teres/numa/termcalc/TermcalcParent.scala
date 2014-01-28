6cd08c69313648033889b0ea32c7fbdffbe1231c
numa08 <n511287@gmail.com>
2014-01-28 22:51:44 +0900

    期間計算タブのピッカーを実装

--- android/DateCalcualtor/src/jp/teres/numa/termcalc/TermcalcParent.scala
+++ android/DateCalcualtor/src/jp/teres/numa/termcalc/TermcalcParent.scala
@@ -5,26 +5,47 @@ import android.view._
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v13.app.FragmentPagerAdapter
+import android.util.Log
 
 import jp.teres.numa.DateCalculator._
+import jp.teres.numa.utils.Conversions._
 
 class TermcalcParent extends Fragment {
 
 	override def onCreateView(inflater : LayoutInflater, container : ViewGroup, savedInstance : Bundle) : View = {
-		val fragments = getResources().getStringArray(R.array.term_calcs).map(SelectDate(_)).toList
-
-		val adapter = new TermcalcParentAdapter(getFragmentManager, fragments)
 		val pager = new ViewPager(getActivity)
-		pager.setAdapter(adapter)
 		pager.setId(R.id.viewPager)
 		pager
+	}
+
+	override def onResume : Unit = {
+		super.onResume
+		val fragments = getResources.getStringArray(R.array.term_calcs).map(SelectDate(_)).toList
+		val adapter = new TermcalcParentAdapter(getFragmentManager, fragments)
+		getActivity.findViewById(R.id.viewPager).asOpt[ViewPager].foreach(_.setAdapter(adapter))
+	}
+
+	override def onStop : Unit = {
+		super.onStop
+		getActivity.findViewById(R.id.viewPager)
+					.asOpt[ViewPager]
+					.map(_.getAdapter)
+					.collect{case a : TermcalcParentAdapter => a}
+					.foreach { a=> 
+						val transaction = getFragmentManager.beginTransaction
+						for( i <- 0 until a.getCount()) {
+							transaction.remove(a.getItem(i))
+						}
+						transaction.commit
+					}
 	}
 }
 
 private class TermcalcParentAdapter(fm : FragmentManager, fragments : List[Fragment]) extends FragmentPagerAdapter(fm) {
 	
 	override def getCount : Int = {
-		fragments.size
+		val c = fragments.size
+		c
 	}
 
 	override def getItem(position: Int) : Fragment = {
