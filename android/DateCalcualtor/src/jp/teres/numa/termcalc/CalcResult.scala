b4206fa8375ed32590e79b38dc0bd414b20c1e40
numa08 <n511287@gmail.com>
2014-02-05 00:35:31 +0900

    ボタン押下で計算をする

--- android/DateCalcualtor/src/jp/teres/numa/termcalc/CalcResult.scala
+++ android/DateCalcualtor/src/jp/teres/numa/termcalc/CalcResult.scala
@@ -4,11 +4,14 @@ import android.app.Fragment
 import android.view._
 import android.widget._
 import android.os.Bundle
+import android.util.Log
 
 import jp.teres.numa.DateCalculator._
 import jp.teres.numa.utils.Conversions._
 
-case class CalcResult extends Fragment {
+case class CalcResult extends Fragment with View.OnClickListener{
+
+	private var showResult : String => Unit = _
 
 	override def onCreateView(inflater : LayoutInflater, container : ViewGroup, savedInstance : Bundle) : View = {
 		val content = inflater.inflate(R.layout.calc_result, null)
@@ -17,12 +20,36 @@ case class CalcResult extends Fragment {
 								   .toString()
 								   .format("2014/01/31", "2014/02/01")
 			tv.setText(text)
+
 		}
 
 		content.findViewById(R.id.result).asOpt[TextView].foreach {tv =>
 			tv.setText("１日")
+			val show = (n : String, tv : TextView) => tv.setText(n.toString)
+			showResult = show(_ : String, tv)
+		}
+
+		content.findViewById(R.id.calc_button).asOpt[Button].foreach {b => 
+			b.setOnClickListener(this)
 		}
 
 		content
+	}
+
+	override def onClick(view : View) : Unit = {
+		calcTerm
+	}
+
+
+	def calcTerm : Unit = {
+		Option(getActivity).collect{ case a : MainActivity => a}
+						   .foreach { act =>
+						   	for{
+						   		start <- act.startDate()
+						   		end   <- act.endDate()
+						   	} yield {
+						   		showResult("hello")
+						   	}
+						   }
 	}
 }
