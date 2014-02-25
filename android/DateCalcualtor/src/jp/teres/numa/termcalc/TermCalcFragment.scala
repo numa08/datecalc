03f89ad87b75f6edbacb9e77daf080e9c60b296a
numa08 <n511287@gmail.com>
2014-02-26 00:31:15 +0900

    期間計算におけるUIコンポーネントの配置と初期化処理の実装

--- android/DateCalcualtor/src/jp/teres/numa/termcalc/TermCalcFragment.scala
+++ android/DateCalcualtor/src/jp/teres/numa/termcalc/TermCalcFragment.scala
@@ -3,8 +3,15 @@ package jp.teres.numa.termcalc
 import android.app.Fragment
 import android.view._
 import android.os.Bundle
+import android.text.format.DateFormat
+import android.widget.TextView
+
+import java.util.Calendar
+
+import org.scaloid.common._
 
 import jp.teres.numa.DateCalculator._
+import jp.teres.numa.utils.Conversions._
 
 class TermCalcFragment extends Fragment {
 	
@@ -12,4 +19,22 @@ class TermCalcFragment extends Fragment {
 		inflater.inflate(R.layout.term_calc, null)
 	}
 
+  override def onViewCreated(view : View, savedInstance : Bundle) : Unit = {
+
+    List(R.id.start_date, 
+        R.id.end_date).map(view.findViewById(_))
+                      .collect{case t : TextView => t}
+                      .map { t=> 
+                        val date = DateFormat.format("yyyy/MM/dd", Calendar.getInstance)
+                        t.setHint(date)
+                        t
+                      }.foreach { t =>
+                        t.onClick(/*open dialog*/)
+                      }
+    view.findViewById(R.id.calc)
+        .asOpt[View]
+        .foreach { b =>
+          b.onClick(/* calc*/)
+        }
+  }
 }
